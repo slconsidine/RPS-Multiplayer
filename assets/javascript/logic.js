c4936ad04bf23cb161756ce473b9e9ec3c51e54b
Sam <samlconsidine@gmail.com>
2020-02-07 12:39:27 -0500

    Added way to see all users in the app. Put player info into cards on HTML.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,6 +13,35 @@ var firebaseConfig = {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     var database = firebase.database();
+
+    // connectionsRef references a specific location in our database.
+    // All of our connections will be stored in this directory.
+    var connectionsRef = database.ref("/connections");
+
+    // '.info/connected' is a special location provided by Firebase that is updated
+    // every time the client's connection state changes.
+    // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+    var connectedRef = database.ref(".info/connected");
+
+    // When the client's connection state changes...
+    connectedRef.on("value", function(snap) {
+
+    // If they are connected..
+        if (snap.val()) {
+            // Add user to the connections list.
+            var con = connectionsRef.push(true);
+            // Remove user from the connection list when they disconnect.
+            con.onDisconnect().remove();
+        }
+    });
+
+    // When first loaded or when the connections list changes...
+    connectionsRef.on("value", function(snap) {
+
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+    $("#connected-viewers").text(snap.numChildren());
+  });
     
     // variables to hold each players choice
     var playerOneChoice;
@@ -145,5 +174,12 @@ var firebaseConfig = {
         console.log("Player 2:" + playerTwoChoice);
         checkWinner();
     });
+
+
+
+
+
+
+
 
 });
