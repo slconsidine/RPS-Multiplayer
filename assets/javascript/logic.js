7768922e6f49c7ac66e7141337db8ce3afee5142
Sam <samlconsidine@gmail.com>
2020-02-07 11:29:32 -0500

    Added win, loss, tie counters and a function to check who the winner is. Still trying to resolve clearing players choices so that the winner check function gets reset correctly.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,7 +3,25 @@ $(document).ready(function() {
     var playerOneChoice;
     var playerTwoChoice;
 
-    // disables al options so they can't be clicked again after a choice is selected
+    // counters for each players
+    // show the initial counters in the HTML
+    var wins1 = 0;
+    var losses1 = 0;
+    var ties1 = 0;
+    $("#wins1").text(wins1);
+    $("#losses1").text(losses1);
+    $("#ties1").text(ties1);
+
+    var wins2 = 0;
+    var losses2 = 0;
+    var ties2 = 0;
+    $("#wins2").text(wins2);
+    $("#losses2").text(losses2);
+    $("#ties2").text(ties2);
+
+
+    // disables all options for each player so they can't be clicked again after a choice is selected
+    // enables all options for all players 
     var playerOneNotEnabled = function() {
         $("#rock").attr("disabled", true);
         $("#paper").attr("disabled", true);
@@ -16,39 +34,95 @@ $(document).ready(function() {
         $("#scissors2").attr("disabled", true);
     };
 
+    var enableAll = function() {
+        $("#rock").attr("disabled", false);
+        $("#paper").attr("disabled", false);
+        $("#scissors").attr("disabled", false); 
+        $("#rock2").attr("disabled", false);
+        $("#paper2").attr("disabled", false);
+        $("#scissors2").attr("disabled", false);
+    };
+    
+    var checkWinner = function() {
+    // if statement confirms that each player has chosen a valid choice before running
+    // if ((playerOneChoice == "rock") || (playerOneChoice == "paper") || (playerOneChoice == "scissors") && (playerTwoChoice == "rock") || (playerTwoChoice == "paper") || (playerTwoChoice == "scissors")) {
+        if ((playerOneChoice != null) && (playerTwoChoice != null)) {
+        console.log("players have picked")
+            // if player 1 is the winner
+            if ((playerOneChoice === "rock" && playerTwoChoice === "scissors") ||
+            (playerOneChoice === "scissors" && playerTwoChoice === "paper") ||
+            (playerOneChoice === "paper" && playerTwoChoice === "rock")) {
+                wins1++;
+                console.log(wins1);
+                $("#wins1").text(wins1);
+                losses2--;
+                console.log(losses2);
+                $("#losses2").text(losses2);
+                enableAll();
+            // if players are tied
+            } else if (playerOneChoice == playerTwoChoice) {
+                ties1++;
+                $("#ties1").text(ties1);
+                ties2++;
+                $("#ties2").text(ties2);
+                enableAll();
+            // if player 2 is the winner
+            } else {
+                losses1++;
+                $("#losses1").text(losses1);
+                wins2++;
+                $("#wins2").text(wins2);
+                enableAll();
+            }
+        }
+    };
+
+
     // when player 1 clicks a button, their choice is logged by grabbing the id of the button
-    // after something is clicked, all other buttons are disabled
+    // after something is clicked, all other buttons for that player are disabled
     $("#rock").on("click", function() {
         playerOneChoice = $(this).attr("id");
         playerOneNotEnabled();
         console.log("Player 1:" + playerOneChoice);
+        checkWinner();
     });
     $("#paper").on("click", function() {
         playerOneChoice = $(this).attr("id");
         playerOneNotEnabled();
         console.log("Player 1:" + playerOneChoice);
+        checkWinner();
     });
     $("#scissors").on("click", function() {
         playerOneChoice = $(this).attr("id");
         playerOneNotEnabled();
         console.log("Player 1:" + playerOneChoice);
+        checkWinner();
     });
 
+    // when player 2 clicks a button, their choice is logged by grabbing the data-value of the button
+    // after something is clicked, all other buttons for that player are disabled
     $("#rock2").on("click", function() {
         playerTwoChoice = $(this).attr("data-value");
         playerTwoNotEnabled();
         console.log("Player 2:" + playerTwoChoice);
+        checkWinner();
     });
     $("#paper2").on("click", function() {
         playerTwoChoice = $(this).attr("data-value");
         playerTwoNotEnabled();
         console.log("Player 2:" + playerTwoChoice);
+        checkWinner();
     });
     $("#scissors2").on("click", function() {
         playerTwoChoice = $(this).attr("data-value");
         playerTwoNotEnabled();
         console.log("Player 2:" + playerTwoChoice);
+        checkWinner();
     });
+
+
+
+
 
 
 
